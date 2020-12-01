8c618e59d346560be9c08ec5df8d3a9ea199672e
Yogitha-Ramisetty <yogitharamisetty05@gmail.com>
2020-12-01 02:23:27 -0500

    Added create and delete operations

--- src/main/java/net/codejava/ClientController.java
+++ src/main/java/net/codejava/ClientController.java
@@ -1,14 +1,43 @@
 package net.codejava;
 import java.util.*;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class ClientController {
+	
 	@Autowired
 	private ClientService service;
-	@GetMapping("/clients")
+	
+
+	// RESTful API method for Create operation
+	@PostMapping("/clientinfo")
+	public void add(@RequestBody Clientinfo clientinfo) {
+	    service.save(clientinfo);
+	}
+	
+	// RESTful API methods for Retrieval operations
+	@GetMapping("/clientinfo")
 	public List<Clientinfo> list(){
 		return service.listAll();
 	}
+	
+	// RESTful API methods for Retrieval operation based on Id
+	@GetMapping("/clientinfo/{id}")
+	public ResponseEntity<Clientinfo> get(@PathVariable Integer id) {
+	    try {
+	        Clientinfo clientinfo = service.get(id);
+	        return new ResponseEntity<Clientinfo>(clientinfo, HttpStatus.OK);
+	    } catch (NoSuchElementException e) {
+	        return new ResponseEntity<Clientinfo>(HttpStatus.NOT_FOUND);
+	    }      
+	}
+	    
+    // RESTful API method for Delete operation
+	@DeleteMapping("/clientinfo/{id}")
+	public void delete(@PathVariable Integer id) {
+		service.delete(id);
+	}
+	
 }
